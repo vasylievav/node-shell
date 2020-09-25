e9beb68be799cba89420749719655c6e4221f209
kmshoell <34169711+kmshoell@users.noreply.github.com>
2020-09-25 15:21:43 -0400

    cat function

--- bash.js
+++ bash.js
@@ -1,5 +1,6 @@
 const pwd = require('./pwd');
 const ls = require('./ls');
+const cat = require('./cat');
 
 
 console.log(typeof process.execPath);
@@ -10,9 +11,11 @@ process.stdin.on('data', (data ) =>{
   const cmd = data.toString().trim();
   if(cmd == 'pwd'){
     pwd();
-  } else 
+  } else
   if (cmd=='ls'){
     ls();
+  } else if (cmd.split(' ')[0] == 'cat'){
+    cat(cmd.split(' ')[1]);
   }
 
   //  pwd()</;
---
+++ cat.js
@@ -0,0 +1,18 @@
+// To implement this command, we'll need to accept not only the name of the command, but also the name of the file that the command will act upon.
+const fs=require('fs');
+
+// Figure out, using fs, how to implement this command for one file argument. The hints below will help, but try to figure it out on your own first.
+
+
+//1. split cmd string
+//2. cat
+//3.  get file contents using fs
+
+module.exports = function cat(name){
+  //get file contents and print to screen
+  fs.readFile(`./${name}`, 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  });
+
+}
