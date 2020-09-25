bb472606d1e75b40183019bd9bf6ac58a9b19f2d
kmshoell <34169711+kmshoell@users.noreply.github.com>
2020-09-25 13:04:47 -0400

    implemented pwd cmd -- created pwd.js file and linked to bash.js file

--- bash.js
+++ bash.js
@@ -1,8 +1,18 @@
+const pwd = require('./pwd');
+
+
+console.log(typeof process.execPath);
+
 process.stdout.write('prompt >');
 
 process.stdin.on('data', (data ) =>{
-const cmd = data.toString().trim();
+  const cmd = data.toString().trim();
+  if(cmd == 'pwd'){
+    pwd();
+  }
 
-process.stdout.write('You typed: ' +cmd);
-process.stdout.write('\nprompt > ');
+  process.stdout.write('\nprompt > ');
 } );
+
+
+
---
+++ pwd.js
@@ -0,0 +1,7 @@
+
+
+module.exports = function pwd(){
+  process.stdout.write(process.execPath);
+}
+
+
